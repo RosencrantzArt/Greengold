551750600dee7812635dc8928ad8f750a2767da2
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-04 08:55:12 +0000

    add secret key

--- green_blog/settings.py
+++ green_blog/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ib--sy))#m90n+1y=fbd$p(%^frqi3+x#pk(y+*^zy3y-kchta'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rosencrantzar-greengold-6ev06u11wm9.ws-eu116.gitpod.io','.herokuapp.com']
 
