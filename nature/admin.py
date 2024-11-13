ebc2bfc751c28917726280c8aab175442b6c6702
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-13 10:27:57 +0000

    fixed bugs and add blogpost

--- nature/admin.py
+++ nature/admin.py
@@ -6,9 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status' created_on')
-    search_fields = ['title' content']
-    list_filter = ('status', 'created_on')
+
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
