fbff477b540ee6462fbe126739afe36b5dd03945
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-13 12:58:27 +0000

    add json code and blog fixtures

--- nature/models.py
+++ nature/models.py
@@ -19,6 +19,8 @@ class Post(models.Model):
     location = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to="post_images/", blank=True, null=True)
     likes = models.ManyToManyField(User, related_name="liked_post", blank=True)
+    excerpt = models.TextField(default="")
+       
 
     def total_likes(self):
         return self.likes.count()
@@ -34,6 +36,8 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
