33d8b6e0fa34522590a53c08ec2c8cd9a6760d09
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-15 08:53:49 +0000

    fixed css and change code for page number function

--- green_blog/settings.py
+++ green_blog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rosencrantzar-greengold-6ev06u11wm9.ws-eu116.gitpod.io','.herokuapp.com']
 
