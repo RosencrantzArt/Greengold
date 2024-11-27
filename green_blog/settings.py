aba553e24f7b5167ae54a19a07ddd4509129cf5b
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-27 05:42:31 +0000

    add style and text to aboute page

--- green_blog/settings.py
+++ green_blog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rosencrantzar-greengold-6ev06u11wm9.ws-eu116.gitpod.io','.herokuapp.com']
 
@@ -57,6 +57,9 @@ INSTALLED_APPS = [
 
 
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
