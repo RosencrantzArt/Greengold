f4d29ee8f828dfe29c7017c5861b1ab164c0aa1e
Anna Rosencrantz <anrosencrantz79@icloud.com>
2024-11-27 05:43:12 +0000

    fixed logout funktion

--- nature/models.py
+++ nature/models.py
@@ -11,14 +11,14 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    featured_image = CloudinaryField('image', blank=True, null=True)  # Tillåt att ingen bild är vald
+    featured_image = CloudinaryField('image', blank=True, null=True)  
     content = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
-    image = models.ImageField(upload_to="post_images/", blank=True, null=True)  # Gör denna också valfri
+    image = models.ImageField(upload_to="post_images/", blank=True, null=True) 
     likes = models.ManyToManyField(User, related_name="liked_post", blank=True)
     excerpt = models.TextField(default="")
        
